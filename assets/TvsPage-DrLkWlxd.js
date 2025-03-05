28fb9e38a06b943dd207ee6f2cd125efaf28837a
dushayevdev <baxtiyorddushaev@gmail.com>
2025-03-05 05:34:58 +0500

    finish-project

---
+++ assets/TvsPage-DrLkWlxd.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./Catalog-CLeeZ1XX.js";import{c as a,a as t,o as s}from"./index-C1o3JTeT.js";const o={class:"main"},p={__name:"TvsPage",setup(c){return(_,r)=>(s(),a("div",o,[t(e,{type:"tv"})]))}};export{p as default};
+//# sourceMappingURL=TvsPage-DrLkWlxd.js.map
